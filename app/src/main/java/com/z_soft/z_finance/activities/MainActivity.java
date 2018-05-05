cd760b0db6b103a231c58430288a753c44137c42
Zvezd04et <trikser@gmail.com>
2018-05-05 23:04:23 +0300

    edit menu of MainActivity

--- app/src/main/java/com/z_soft/z_finance/activities/MainActivity.java
+++ app/src/main/java/com/z_soft/z_finance/activities/MainActivity.java
@@ -85,17 +85,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_operations) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_sources) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_storages) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_reports) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_help) {
 
         }
 
